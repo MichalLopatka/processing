398278bcdac913d558a721e235084beccbdb7f94
MichalLopatka <michalblopatka@gmail.com>
2022-12-16 17:43:55 +0100

    added comunicating with source

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    pass
+from processing.producer import Producer
 
+def main():
+    producer = Producer()
+    frame = producer.download_frame()
+    print(frame)
 
 if __name__ == "__main__":
     main()
--- processing/producer.py
+++ processing/producer.py
@@ -1,2 +1,10 @@
+from processing.source import Source
 
+class Producer:
+    def __init__(self, rows: int=1024, cols:int=768, channels:int=3):
+        self.source_shape = (rows, cols, channels)
+        self.source = Source(self.source_shape)
 
+    def download_frame(self):
+        frame = self.source.get_data()
+        return frame
